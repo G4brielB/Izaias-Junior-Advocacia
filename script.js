2a6128026c02df40a95c96045bd5f89e57bd7d63
Gabriel de Carvalho Silva <g4brieldcs@gmail.com>
2026-05-11 09:28:26 -0300

    Novidades: balão de chat, carrosel de post instagram e correções em monitores full hd

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
 const elements = {
-    openWhatsapp: document.querySelector("#openWhatsapp"),
     sectionPages: document.querySelectorAll(".hidden"),
-    btnRedirect: document.querySelectorAll('.redirect')
+    btnRedirect: document.querySelectorAll('.redirect'),
+    btnChat: document.querySelector(".btn-chat"),
+    interfaceChat: document.querySelector(".interface-chat"),
+    closeChat: document.querySelector(".close"),
+    textWhatsapp: document.querySelector(".text-whatsapp"),
+    btnTextWhatsapp: document.querySelector("#btnTextWhatsapp")
 }
 
 const links = {
-    linkedin: "",
-    instagram: "",
+    linkedin: "https://www.linkedin.com/in/dr-izaias-barros-junior/",
+    instagram: "https://www.instagram.com/izzaiasjr",
     whatsapp: "http://wa.me/+5534984380565"
 }
 
@@ -16,6 +20,26 @@ function redirectSite () {
             let button = item.target.dataset.link
             window.open(links[button], "_blank")
         })
+    })
+}
+
+function showChat () {
+    elements.btnChat.addEventListener('click', () => {
+        elements.interfaceChat.classList.add('active')
+    })
+
+    elements.closeChat.addEventListener("click", () => {
+        elements.interfaceChat.classList.remove("active")
+    })
+}
+
+function sendMessageWhatsapp () {
+    elements.btnTextWhatsapp.addEventListener("click", e => {
+        e.preventDefault()
+        const text = elements.textWhatsapp.value
+        const message = encodeURIComponent(`${text}`)
+        const messagelink = `http://wa.me/+5534984380565?text="${message}"`
+        window.open(messagelink, '_blank')
     })
 }
 
@@ -38,7 +62,9 @@ function showSections () {
 
 function init () {
     redirectSite()
+    sendMessageWhatsapp()
     showSections()
+    showChat()
 }
 
 init()
